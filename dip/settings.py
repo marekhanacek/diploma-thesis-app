aedd8eddc2d4f834688b92028fc658cc15e79b07
Marek Hanáček <marekhanacek1@gmail.com>
2017-05-02 19:01:49 +0200

    fix of static files

--- dip/settings.py
+++ dip/settings.py
@@ -132,7 +132,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
