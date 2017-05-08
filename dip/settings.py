b474f30ee0b27e6d9f4082c51a2887a040e8e973
Marek Hanáček <marekhanacek1@gmail.com>
2017-05-08 20:00:30 +0200

    static_root

--- dip/settings.py
+++ dip/settings.py
@@ -134,7 +134,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
