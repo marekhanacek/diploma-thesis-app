ee45b1f322a6d51171b0d3d526f8b6354ceee3a6
Marek Hanáček <marekhanacek1@gmail.com>
2017-05-08 20:29:23 +0200

    static fix

--- dip/settings.py
+++ dip/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
